c15355e741f417e1d1cf6eda094c0a539744ff2b
skwms <skwms1223@naver.com>
2022-03-04 21:54:25 +0900

    백준 문제 풀이

---
+++ src/BAEK_8958.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEK_8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = br.readLine();
+        }
+
+        for(int i =0; i < N; i++){
+            int count = 0;
+            int sum = 0;
+
+            for(int j = 0; j < arr[i].length(); j++){
+                if(arr[i].charAt(j) == 'O'){
+                    count++;
+                }
+                else{
+                    count = 0;
+                }
+                sum = sum + count;
+            }
+            sb.append(sum).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
